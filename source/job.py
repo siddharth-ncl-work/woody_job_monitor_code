5849df3061001fa4e2da607d9c2e1cd12db36a09
ss.ghule <ss.ghule@ncl.res.in>
2020-01-08 17:37:21 +0530

    rename repo woody_job_monitor_code --> woody_job_monitor_script this is a test commit to test connection with remote repo

--- source/job.py
+++ source/job.py
@@ -101,7 +101,7 @@ def runJob(prev_dir_name,nxt_dir_name,steps=list(range(1,10))):
 
   #step_1
   if 1 in steps:
-    print('step 1 running...')
+    print('STEP 1 running...')
     prev_file_path=os.path.join(prev_dir_path,'coord')
     new_file_path=os.path.join(prev_dir_path,prev_dir_name+'.xyz')
     print(f't2x converting {prev_file_path} to {new_file_path}...') 
@@ -110,20 +110,20 @@ def runJob(prev_dir_name,nxt_dir_name,steps=list(range(1,10))):
     print(f'creating directory {nxt_dir_path}...')
     p=subprocess.run(['mkdir',nxt_dir_path])  
     checkProcess(p)
-    print('step 1 finished')
+    print('STEP 1 finished')
 
   #step_2
   if 2 in steps:
-    print('step 2 running...')
+    print('STEP 2 running...')
     prev_file_path=os.path.join(prev_dir_path,prev_dir_name+'.xyz') 
     new_file_path=os.path.join(nxt_dir_path,nxt_dir_name+'.xyz')
     print('stetching bonds...')
     stretch_bond.stretchBond(prev_file_path,outfile_path=new_file_path,atom0=config.atom0,atom1=config.atom1,moiety=config.moiety,trans=config.step_size)
-    print('step 2 finished')
+    print('STEP 2 finished')
 
   #step_3
   if 3 in steps:
-    print('step 3 running...')
+    print('STEP 3 running...')
     prev_file_path=os.path.join(nxt_dir_path,nxt_dir_name+'.xyz')
     new_file_path=os.path.join(nxt_dir_path,'coord')
     print(f'x2t converting {prev_file_path} to {new_file_path}...')
@@ -133,58 +133,58 @@ def runJob(prev_dir_name,nxt_dir_name,steps=list(range(1,10))):
     checkProcess(p) 
     print(f'fixing atoms {config.atom0},{config.atom1}...')
     fixAtoms(new_file_path,atoms_list=[config.atom0,config.atom1])
-    print('step 3 finished')              
+    print('STEP 3 finished')              
 
   #step_4(copy files)
   if 4 in steps:
-    print('step 4 running...')
+    print('STEP 4 running...')
     print(f'copying files from {config.common_files_dir_abs_path}')
     common_files_dir_abs_path=os.path.join(config.common_files_dir_abs_path,'.')
     p=subprocess.run(['cp','-a',common_files_dir_abs_path,nxt_dir_path]) 
     checkProcess(p)
-    print('step 4 finished')
+    print('STEP 4 finished')
 
   #step_5
   if 5 in steps:
-    print('step 5 running...')
+    print('STEP 5 running...')
     print('modifying control file...')
     file_path=os.path.join(nxt_dir_path,'control')
     modifyControlFile(file_path,nxt_dir_name)
-    print('step 5 finished')
+    print('STEP 5 finished')
 
   #step_6
   if 6 in steps:
-    print('step 6 running...')
+    print('STEP 6 running...')
     print('modifying try_script file...')
     file_path=os.path.join(nxt_dir_path,'try_script')
     modifyTryScript(file_path,nxt_dir_name)
-    print('step 6 finished')
+    print('STEP 6 finished')
 
   #step_7(run_try_script)
   if 7 in steps:
-    print('step 7 running...')
+    print('STEP 7 running...')
     print('running try_script...')
     p=subprocess.run(['./try_script'],cwd=nxt_dir_path)
     checkProcess(p)    
-    print('step 7 finished')
+    print('STEP 7 finished')
 
   #step_8
   if 8 in steps:
-    print('step 8 running...')
+    print('STEP 8 running...')
     print('modifying opt_woody_script file...')
     file_path=os.path.join(nxt_dir_path,'opt_woody_script')
     modifyOptScript(file_path,nxt_dir_name)
-    print('step 8 finished')
+    print('STEP 8 finished')
 
   #step_9
   if 9 in steps:
-    print('step 9 running...')
+    print('STEP 9 running...')
     print(f'submitting qsub job: {nxt_dir_name}')
     p=subprocess.run(['qsub','opt_woody_script'],cwd=nxt_dir_path)
     checkProcess(p)
     print(f'Waiting for job {nxt_dir_name} to finish...')
     wait.wait(nxt_dir_name)
-    print('step 9 finished')
+    print('STEP 9 finished')
   
 if __name__=='__main__':
   prev_dir_name='2.7'
